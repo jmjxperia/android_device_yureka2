b26db016d5dadd1c8862d325dc4082ec790e850b
jmjxperia <jmjxperia@users.noreply.github.com>
2019-01-14 15:53:40 +0530

    doze:adapt to lineage

--- doze/Android.mk
+++ doze/Android.mk
@@ -17,10 +17,14 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
-    android-support-v14-preference
+    android-support-v14-preference \
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
