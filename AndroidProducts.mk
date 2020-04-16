13024f12823082cbedd6a57355aab12bd1cd25c2
jmjxperia <jmjxperia@users.noreply.github.com>
2020-04-16 13:44:27 +0530

    ancient bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_YUREKA2.mk
+    $(LOCAL_DIR)/ancient_YUREKA2.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_YUREKA2-user \
-    havoc_YUREKA2-userdebug \
-    havoc_YUREKA2-eng
+    ancient_YUREKA2-user \
+    ancient_YUREKA2-userdebug \
+    ancient_YUREKA2-eng
