cdaa10947b1dfafa7d332c222924b581a0669a7c
jmjxperia <jathinjater1996@gmail.com>
2020-04-17 12:15:00 +0530

    YUREKA2: add TARGET_GAPPS_ARCH

--- ancient_YUREKA2.mk
+++ ancient_YUREKA2.mk
@@ -27,6 +27,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_ARCH := arm64
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := ancient_YUREKA2
